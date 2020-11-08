18801352548f00d806202f203412750cbf2fc991
Quân Nguyễn <nquan146@gmail.com>
2020-11-08 17:55:58 +0700

    lát sửa tiếp

--- Project_WInForm/Project_WInForm/HoaDon.Designer.cs
+++ Project_WInForm/Project_WInForm/HoaDon.Designer.cs
@@ -31,26 +31,26 @@
             this.pnFooter = new System.Windows.Forms.Panel();
             this.btDatMua = new System.Windows.Forms.Button();
             this.pnContain = new System.Windows.Forms.Panel();
-            this.pnPhuongThucThanhToan = new System.Windows.Forms.Panel();
             this.pnThongTin = new System.Windows.Forms.Panel();
-            this.lbTinhThanhPho = new System.Windows.Forms.Label();
-            this.txtHoTen = new System.Windows.Forms.TextBox();
-            this.lbDiaChiGiaoHang = new System.Windows.Forms.Label();
+            this.rtxtDiaChi = new System.Windows.Forms.RichTextBox();
+            this.cbxQuanHuyen = new System.Windows.Forms.ComboBox();
+            this.cbxTinhThanhPho = new System.Windows.Forms.ComboBox();
+            this.txtDienThoaiDiDong = new System.Windows.Forms.TextBox();
+            this.lbDiaChi = new System.Windows.Forms.Label();
+            this.lbQuanHuyen = new System.Windows.Forms.Label();
+            this.lbHoTen = new System.Windows.Forms.Label();
             this.lbDienThoaiDiDong = new System.Windows.Forms.Label();
+            this.lbDiaChiGiaoHang = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.lbTinhThanhPho = new System.Windows.Forms.Label();
+            this.pnPhuongThucThanhToan = new System.Windows.Forms.Panel();
+            this.rbtThanhToanThe = new System.Windows.Forms.RadioButton();
             this.lbPhuongThucThanhToan = new System.Windows.Forms.Label();
             this.rbtThanhToanKhiGiaoHang = new System.Windows.Forms.RadioButton();
-            this.rbtThanhToanThe = new System.Windows.Forms.RadioButton();
-            this.lbHoTen = new System.Windows.Forms.Label();
-            this.lbQuanHuyen = new System.Windows.Forms.Label();
-            this.lbDiaChi = new System.Windows.Forms.Label();
-            this.txtDienThoaiDiDong = new System.Windows.Forms.TextBox();
-            this.cbxTinhThanhPho = new System.Windows.Forms.ComboBox();
-            this.cbxQuanHuyen = new System.Windows.Forms.ComboBox();
-            this.rtxtDiaChi = new System.Windows.Forms.RichTextBox();
             this.pnFooter.SuspendLayout();
             this.pnContain.SuspendLayout();
-            this.pnPhuongThucThanhToan.SuspendLayout();
             this.pnThongTin.SuspendLayout();
+            this.pnPhuongThucThanhToan.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnFooter
@@ -84,17 +84,6 @@
             this.pnContain.Size = new System.Drawing.Size(496, 463);
             this.pnContain.TabIndex = 2;
             // 
-            // pnPhuongThucThanhToan
-            // 
-            this.pnPhuongThucThanhToan.Controls.Add(this.rbtThanhToanThe);
-            this.pnPhuongThucThanhToan.Controls.Add(this.lbPhuongThucThanhToan);
-            this.pnPhuongThucThanhToan.Controls.Add(this.rbtThanhToanKhiGiaoHang);
-            this.pnPhuongThucThanhToan.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnPhuongThucThanhToan.Location = new System.Drawing.Point(0, 390);
-            this.pnPhuongThucThanhToan.Name = "pnPhuongThucThanhToan";
-            this.pnPhuongThucThanhToan.Size = new System.Drawing.Size(496, 73);
-            this.pnPhuongThucThanhToan.TabIndex = 1;
-            // 
             // pnThongTin
             // 
             this.pnThongTin.Controls.Add(this.rtxtDiaChi);
@@ -114,21 +103,72 @@
             this.pnThongTin.Size = new System.Drawing.Size(496, 390);
             this.pnThongTin.TabIndex = 2;
             // 
-            // lbTinhThanhPho
+            // rtxtDiaChi
             // 
-            this.lbTinhThanhPho.AutoSize = true;
-            this.lbTinhThanhPho.Location = new System.Drawing.Point(36, 118);
-            this.lbTinhThanhPho.Name = "lbTinhThanhPho";
-            this.lbTinhThanhPho.Size = new System.Drawing.Size(86, 13);
-            this.lbTinhThanhPho.TabIndex = 0;
-            this.lbTinhThanhPho.Text = "Tỉnh/Thành Phố";
+            this.rtxtDiaChi.Location = new System.Drawing.Point(167, 195);
+            this.rtxtDiaChi.Name = "rtxtDiaChi";
+            this.rtxtDiaChi.Size = new System.Drawing.Size(192, 48);
+            this.rtxtDiaChi.TabIndex = 11;
+            this.rtxtDiaChi.Text = "";
             // 
-            // txtHoTen
+            // cbxQuanHuyen
             // 
-            this.txtHoTen.Location = new System.Drawing.Point(167, 39);
-            this.txtHoTen.Name = "txtHoTen";
-            this.txtHoTen.Size = new System.Drawing.Size(192, 20);
-            this.txtHoTen.TabIndex = 1;
+            this.cbxQuanHuyen.FormattingEnabled = true;
+            this.cbxQuanHuyen.Location = new System.Drawing.Point(167, 152);
+            this.cbxQuanHuyen.Name = "cbxQuanHuyen";
+            this.cbxQuanHuyen.Size = new System.Drawing.Size(165, 21);
+            this.cbxQuanHuyen.TabIndex = 10;
+            // 
+            // cbxTinhThanhPho
+            // 
+            this.cbxTinhThanhPho.FormattingEnabled = true;
+            this.cbxTinhThanhPho.Location = new System.Drawing.Point(167, 115);
+            this.cbxTinhThanhPho.Name = "cbxTinhThanhPho";
+            this.cbxTinhThanhPho.Size = new System.Drawing.Size(165, 21);
+            this.cbxTinhThanhPho.TabIndex = 9;
+            // 
+            // txtDienThoaiDiDong
+            // 
+            this.txtDienThoaiDiDong.Location = new System.Drawing.Point(167, 77);
+            this.txtDienThoaiDiDong.Name = "txtDienThoaiDiDong";
+            this.txtDienThoaiDiDong.Size = new System.Drawing.Size(165, 20);
+            this.txtDienThoaiDiDong.TabIndex = 8;
+            // 
+            // lbDiaChi
+            // 
+            this.lbDiaChi.AutoSize = true;
+            this.lbDiaChi.Location = new System.Drawing.Point(34, 198);
+            this.lbDiaChi.Name = "lbDiaChi";
+            this.lbDiaChi.Size = new System.Drawing.Size(41, 13);
+            this.lbDiaChi.TabIndex = 7;
+            this.lbDiaChi.Text = "Địa Chỉ";
+            // 
+            // lbQuanHuyen
+            // 
+            this.lbQuanHuyen.AutoSize = true;
+            this.lbQuanHuyen.Location = new System.Drawing.Point(34, 155);
+            this.lbQuanHuyen.Name = "lbQuanHuyen";
+            this.lbQuanHuyen.Size = new System.Drawing.Size(69, 13);
+            this.lbQuanHuyen.TabIndex = 5;
+            this.lbQuanHuyen.Text = "Quận/Huyện";
+            // 
+            // lbHoTen
+            // 
+            this.lbHoTen.AutoSize = true;
+            this.lbHoTen.Location = new System.Drawing.Point(34, 42);
+            this.lbHoTen.Name = "lbHoTen";
+            this.lbHoTen.Size = new System.Drawing.Size(43, 13);
+            this.lbHoTen.TabIndex = 4;
+            this.lbHoTen.Text = "Họ Tên";
+            // 
+            // lbDienThoaiDiDong
+            // 
+            this.lbDienThoaiDiDong.AutoSize = true;
+            this.lbDienThoaiDiDong.Location = new System.Drawing.Point(30, 80);
+            this.lbDienThoaiDiDong.Name = "lbDienThoaiDiDong";
+            this.lbDienThoaiDiDong.Size = new System.Drawing.Size(104, 13);
+            this.lbDienThoaiDiDong.TabIndex = 3;
+            this.lbDienThoaiDiDong.Text = " Điện Thoại Di Động";
             // 
             // lbDiaChiGiaoHang
             // 
@@ -140,14 +180,44 @@
             this.lbDiaChiGiaoHang.TabIndex = 2;
             this.lbDiaChiGiaoHang.Text = "2.Địa Chỉ Giao Hàng";
             // 
-            // lbDienThoaiDiDong
+            // txtHoTen
             // 
-            this.lbDienThoaiDiDong.AutoSize = true;
-            this.lbDienThoaiDiDong.Location = new System.Drawing.Point(36, 80);
-            this.lbDienThoaiDiDong.Name = "lbDienThoaiDiDong";
-            this.lbDienThoaiDiDong.Size = new System.Drawing.Size(104, 13);
-            this.lbDienThoaiDiDong.TabIndex = 3;
-            this.lbDienThoaiDiDong.Text = " Điện Thoại Di Động";
+            this.txtHoTen.Location = new System.Drawing.Point(167, 39);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(192, 20);
+            this.txtHoTen.TabIndex = 1;
+            // 
+            // lbTinhThanhPho
+            // 
+            this.lbTinhThanhPho.AutoSize = true;
+            this.lbTinhThanhPho.Location = new System.Drawing.Point(32, 118);
+            this.lbTinhThanhPho.Name = "lbTinhThanhPho";
+            this.lbTinhThanhPho.Size = new System.Drawing.Size(86, 13);
+            this.lbTinhThanhPho.TabIndex = 0;
+            this.lbTinhThanhPho.Text = "Tỉnh/Thành Phố";
+            // 
+            // pnPhuongThucThanhToan
+            // 
+            this.pnPhuongThucThanhToan.Controls.Add(this.rbtThanhToanThe);
+            this.pnPhuongThucThanhToan.Controls.Add(this.lbPhuongThucThanhToan);
+            this.pnPhuongThucThanhToan.Controls.Add(this.rbtThanhToanKhiGiaoHang);
+            this.pnPhuongThucThanhToan.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnPhuongThucThanhToan.Location = new System.Drawing.Point(0, 390);
+            this.pnPhuongThucThanhToan.Name = "pnPhuongThucThanhToan";
+            this.pnPhuongThucThanhToan.Size = new System.Drawing.Size(496, 73);
+            this.pnPhuongThucThanhToan.TabIndex = 1;
+            // 
+            // rbtThanhToanThe
+            // 
+            this.rbtThanhToanThe.AutoSize = true;
+            this.rbtThanhToanThe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtThanhToanThe.Location = new System.Drawing.Point(39, 50);
+            this.rbtThanhToanThe.Name = "rbtThanhToanThe";
+            this.rbtThanhToanThe.Size = new System.Drawing.Size(150, 19);
+            this.rbtThanhToanThe.TabIndex = 5;
+            this.rbtThanhToanThe.TabStop = true;
+            this.rbtThanhToanThe.Text = "Thanh Toán Bằng Thẻ ";
+            this.rbtThanhToanThe.UseVisualStyleBackColor = true;
             // 
             // lbPhuongThucThanhToan
             // 
@@ -171,76 +241,6 @@
             this.rbtThanhToanKhiGiaoHang.Text = " Thanh toán tiền mặt khi giao hàng";
             this.rbtThanhToanKhiGiaoHang.UseVisualStyleBackColor = true;
             // 
-            // rbtThanhToanThe
-            // 
-            this.rbtThanhToanThe.AutoSize = true;
-            this.rbtThanhToanThe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbtThanhToanThe.Location = new System.Drawing.Point(39, 50);
-            this.rbtThanhToanThe.Name = "rbtThanhToanThe";
-            this.rbtThanhToanThe.Size = new System.Drawing.Size(150, 19);
-            this.rbtThanhToanThe.TabIndex = 5;
-            this.rbtThanhToanThe.TabStop = true;
-            this.rbtThanhToanThe.Text = "Thanh Toán Bằng Thẻ ";
-            this.rbtThanhToanThe.UseVisualStyleBackColor = true;
-            // 
-            // lbHoTen
-            // 
-            this.lbHoTen.AutoSize = true;
-            this.lbHoTen.Location = new System.Drawing.Point(36, 42);
-            this.lbHoTen.Name = "lbHoTen";
-            this.lbHoTen.Size = new System.Drawing.Size(43, 13);
-            this.lbHoTen.TabIndex = 4;
-            this.lbHoTen.Text = "Họ Tên";
-            // 
-            // lbQuanHuyen
-            // 
-            this.lbQuanHuyen.AutoSize = true;
-            this.lbQuanHuyen.Location = new System.Drawing.Point(38, 155);
-            this.lbQuanHuyen.Name = "lbQuanHuyen";
-            this.lbQuanHuyen.Size = new System.Drawing.Size(69, 13);
-            this.lbQuanHuyen.TabIndex = 5;
-            this.lbQuanHuyen.Text = "Quận/Huyện";
-            // 
-            // lbDiaChi
-            // 
-            this.lbDiaChi.AutoSize = true;
-            this.lbDiaChi.Location = new System.Drawing.Point(38, 198);
-            this.lbDiaChi.Name = "lbDiaChi";
-            this.lbDiaChi.Size = new System.Drawing.Size(41, 13);
-            this.lbDiaChi.TabIndex = 7;
-            this.lbDiaChi.Text = "Địa Chỉ";
-            // 
-            // txtDienThoaiDiDong
-            // 
-            this.txtDienThoaiDiDong.Location = new System.Drawing.Point(167, 77);
-            this.txtDienThoaiDiDong.Name = "txtDienThoaiDiDong";
-            this.txtDienThoaiDiDong.Size = new System.Drawing.Size(165, 20);
-            this.txtDienThoaiDiDong.TabIndex = 8;
-            // 
-            // cbxTinhThanhPho
-            // 
-            this.cbxTinhThanhPho.FormattingEnabled = true;
-            this.cbxTinhThanhPho.Location = new System.Drawing.Point(167, 115);
-            this.cbxTinhThanhPho.Name = "cbxTinhThanhPho";
-            this.cbxTinhThanhPho.Size = new System.Drawing.Size(165, 21);
-            this.cbxTinhThanhPho.TabIndex = 9;
-            // 
-            // cbxQuanHuyen
-            // 
-            this.cbxQuanHuyen.FormattingEnabled = true;
-            this.cbxQuanHuyen.Location = new System.Drawing.Point(167, 152);
-            this.cbxQuanHuyen.Name = "cbxQuanHuyen";
-            this.cbxQuanHuyen.Size = new System.Drawing.Size(165, 21);
-            this.cbxQuanHuyen.TabIndex = 10;
-            // 
-            // rtxtDiaChi
-            // 
-            this.rtxtDiaChi.Location = new System.Drawing.Point(167, 195);
-            this.rtxtDiaChi.Name = "rtxtDiaChi";
-            this.rtxtDiaChi.Size = new System.Drawing.Size(192, 48);
-            this.rtxtDiaChi.TabIndex = 11;
-            this.rtxtDiaChi.Text = "";
-            // 
             // HoaDon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -252,10 +252,10 @@
             this.Text = "HoaDon";
             this.pnFooter.ResumeLayout(false);
             this.pnContain.ResumeLayout(false);
-            this.pnPhuongThucThanhToan.ResumeLayout(false);
-            this.pnPhuongThucThanhToan.PerformLayout();
             this.pnThongTin.ResumeLayout(false);
             this.pnThongTin.PerformLayout();
+            this.pnPhuongThucThanhToan.ResumeLayout(false);
+            this.pnPhuongThucThanhToan.PerformLayout();
             this.ResumeLayout(false);
 
         }
